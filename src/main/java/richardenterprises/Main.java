e38e1c844fa4871fbd1010e72ab3de5de0595da9
Richard Dsouza <richarddsouza12@gmail.com>
2023-11-13 11:42:50 +0530

    Commit 2

--- src/main/java/richardenterprises/Main.java
+++ src/main/java/richardenterprises/Main.java
@@ -3,5 +3,12 @@ package richardenterprises;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+
+        System.out.println("Hello world!");
+
     }
 }
